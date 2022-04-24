7eaa799650afa578caf2a0c87576e2528f29e6b3
Matteo Lecca <matteolecca@MacBook-Pro-di-Matteo.local>
2022-04-24 12:40:36 +0200

    updated import router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = new express.Router()
-const userRouter = require('./user-router.ts');
-const bookingsRouter = require('./bookings-router.ts');
-const notificationsRouter = require('./notification-router.ts');
+const userRouter = require('./user-router');
+const bookingsRouter = require('./bookings-router');
+const notificationsRouter = require('./notification-router');
 router.use('/users', userRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/notifications', notificationsRouter);
